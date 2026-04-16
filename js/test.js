6f81899f17fb45c6a362f7cb49fdb28a04b76cfc
mehedi6463 <mehedi6463@gmail.com>
2026-04-16 17:55:16 +0600

    updated

--- js/test.js
+++ js/test.js
@@ -2,8 +2,27 @@ function nameMy() {
     document.getElementById('teste').innerHTML='test update';
     
 }
-// finction with parameter
+// function with parameter
 function sleepTime(nam, time) {
     console.log(nam + " is sleeping from " + time);    
 }
+sleepTime("mahedi", "9pm");
+sleepTime("hasan", "12am");
+
+// function with return
+function myName(x,y) {
+    return x+y;
+}
+let z=myName(3,6);
+console.log(z);
+
+// function
+function sayHello() {
+  return "Hello World This is JS";
+}
+
+let greeting = sayHello();
+
+document.getElementById("demo").innerHTML = greeting;
+
 
