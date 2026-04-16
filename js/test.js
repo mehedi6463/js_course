7fd4d504d1de43eccbabdbc877ef0f5175fdfb0c
mehedi6463 <mehedi6463@gmail.com>
2026-04-16 16:07:22 +0600

    add new function

--- js/test.js
+++ js/test.js
@@ -2,3 +2,9 @@ function nameMy() {
     document.getElementById('teste').innerHTML='test update';
     
 }
+
+function myFunction1() {
+    document.getElementById('function').innerHTML='its a function';
+    
+    
+}
