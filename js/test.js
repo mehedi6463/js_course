00f30e80851371107f273303e066cd8ff21429b1
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-04-18 23:07:27 +0600

    js update

--- js/test.js
+++ js/test.js
@@ -1,17 +1,17 @@
 function nameMy() {
-    document.getElementById('teste').innerHTML='test update';
-    
+    document.getElementById('teste').innerHTML = 'test update';
+
 }
 // function with parameter
 function sleepTime(nam, time) {
-    console.log(nam + " is sleeping from " + time);    
+    console.log(nam + " is sleeping from " + time);
 }
 sleepTime("mahedi", "9pm");
 sleepTime("hasan", "12am");
 
 // function with return
-function myName(x,y) {
-    return x+y;
+function myName(x, y) {
+    return x + y;
 }
 // let z=myName(3,6);
 // console.log(myName(3,8));
@@ -38,6 +38,9 @@ function myName(x,y) {
 // };
 // console.log(car.name);
 
+//onclick event
 
+function displayDate() {
+    document.getElementById("demo000").innerHTML = Date();
 
-
+}
