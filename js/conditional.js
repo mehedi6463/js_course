30547ac82e513beb85217cdc6a3f9520d0576243
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-05-10 23:11:55 +0600

    update

--- js/conditional.js
+++ js/conditional.js
@@ -1,13 +1,47 @@
-       break;
+//if else condition...
+// let age =3;
 
-   default:
-        day="Not Found";
+// if (age > 18) {
+//     console.log("You are Adult");  
+// }else if (age ==18 &&  ){
+//     console.log("Adult age");
+// }else if(age>18 && age<30)
+// console.log("Matured age");
+// else{
+//     console.log("You are Child");
+// }
+
+//switch case using date object...
+/*
+let day;
+let data= new Date().getDay();
+
+switch (data) {
+    case 0:
+        day="Sunday";        
         break;
-}
-document.getElementById("demo").innerHTML="Today is " + day;
-console.log(day);
-       day="NOT FOUND";
-       break;
+    case 1:
+        day="Monday";        
+        break;
+    case 2:
+        day="Tuesday";        
+        break;
+    case 3:
+        day="Wednesday";        
+        break;
+    case 4:
+        day="Thursday";        
+        break;
+    case 5:
+        day="FriDay";        
+        break;
+    case 6:
+        day="Saturday";        
+        break;
+
+    default:
+        day="NOT FOUND";
+        break;
 }
 document.getElementById("demo").innerHTML="Day is " + day;
 */
@@ -20,9 +54,9 @@ let cars=['BMW', "Rols Royals", "Lamborgini", "Corola", "Saab", "Volvo", "Ford"]
 // for (let i = 0; i < cars.length; i++) {
 //     let carSorted =cars.toSorted();
 //     console.log(carSorted[i]);
-for (let i = 0; i < cars.length; i++) {
-    let carSorted =cars.toSorted();
-    console.log(carSorted[i]);
+// for (let i = 0; i < cars.length; i++) {
+//     let carSorted =cars.toSorted();
+//     console.log(carSorted[i]);
 
 // }
 // console.log("its done...");
