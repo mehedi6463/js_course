2a1c7527d797ec6473e82edadc0a766630b887c4
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-05-10 23:06:42 +0600

    update

--- js/conditional.js
+++ js/conditional.js
@@ -1,57 +1,73 @@
-//if else condition...
-// let age =3;
+       break;
 
-// if (age > 18) {
-//     console.log("You are Adult");  
-// }else if (age ==18 &&  ){
-//     console.log("Adult age");
-// }else if(age>18 && age<30)
-// console.log("Matured age");
-// else{
-//     console.log("You are Child");
-// }
-
-//switch case using date object...
-/*
-let day;
-let data= new Date().getDay();
-
-switch (data) {
-    case 0:
-        day="Sunday";        
+   default:
+        day="Not Found";
         break;
-    case 1:
-        day="Monday";        
-        break;
-    case 2:
-        day="Tuesday";        
-        break;
-    case 3:
-        day="Wednesday";        
-        break;
-    case 4:
-        day="Thursday";        
-        break;
-    case 5:
-        day="FriDay";        
-        break;
-    case 6:
-        day="Saturday";        
-        break;
-
-    default:
-        day="NOT FOUND";
-        break;
+}
+document.getElementById("demo").innerHTML="Today is " + day;
+console.log(day);
+       day="NOT FOUND";
+       break;
 }
 document.getElementById("demo").innerHTML="Day is " + day;
 */
 
+//for loop in array with alphabetically sorted...
+// let cars=['BMW', "Rolls-Royce", "Lamborghini", "Corolla", "Saab", "Volvo", "Ford"];
 //for loop in array with alphabetic sorted...
 let cars=['BMW', "Rols Royals", "Lamborgini", "Corola", "Saab", "Volvo", "Ford"];
 
+// for (let i = 0; i < cars.length; i++) {
+//     let carSorted =cars.toSorted();
+//     console.log(carSorted[i]);
 for (let i = 0; i < cars.length; i++) {
     let carSorted =cars.toSorted();
     console.log(carSorted[i]);
-    
+
+// }
+// console.log("its done...");
+
+//For loop
+/*
+const cars =["BMW", "Audi", "Volvo", "Rolls-Royce"];
+let text ="";
+
+for(i=0; i<cars.length; i++){
+let carSort=cars.toSorted();
+text += carSort[i]+"<br>";
 }
+
+document.getElementById("demo").innerHTML=text;
+*/
+
+//For in loop with object... recommended for OBJECT
+/*
+const person = {fname:"Mehedi",lname: "Hasan", age:30};
+let text ="";
+
+for(let x in person){
+//console.log(x);
+text += person[x] + " " ;
+}
+document.getElementById("demo").innerHTML=text;
+*/
+
+//For in loop with array... for in loop is not recommended for array
+/*
+const number =[2, 5, 7, 1, 6, 3, 9, 4];
+
+let text ="";
+for (let x in number){
+    let numSort=number.toSorted();
+    console.log(numSort[x]);
+}
+*/
+
+//For OF loop with array... recommended for array
+/*
+const number =[2, 5, 7, 1, 6, 3, 9, 4];
+for (let x of number){
+    console.log(x);
+}
+*/
 console.log("its done...");
