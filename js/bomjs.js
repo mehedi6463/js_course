31b7c2d97da2528ba809f1c3156c859acd76eb75
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-08-26 00:52:27 +0600

    AlertConfirPromtBox

--- js/bomjs.js
+++ js/bomjs.js
@@ -1,17 +1,50 @@
 let myWind;
 
 // select div id
-const width =document.getElementById('p1');
-const height =document.getElementById('p2');
+// const width =document.getElementById('p1');
+// const height =document.getElementById('p2');
 
-width.innerHTML = "inner window width is :" + window.innerWidth;
-height.innerHTML = "inner window height is :" + window.innerHeight;
+// width.innerHTML = "inner window width is :" + window.innerWidth;
+// height.innerHTML = "inner window height is :" + window.innerHeight;
 
-function openWind() {
-    myWind = window.open("https://jsoneditoronline.org");
+// function openWind() {
+//     myWind = window.open("https://jsoneditoronline.org");
+    
+// }
+
+// function closeWind() {
+//     myWind.close();
+// }
+
+//select for showing massage
+const clickFor =document.getElementById("p3");
+const clickProm =document.getElementById("p4");
+
+//popup alert box
+function windAlert() {
+    alert('Window Alert is working');
+}
+
+function windConfirm() {
+    let txt;
+    if (confirm("Press ok for confirm")) {
+        txt = "Done..!";
+    }else{
+        txt = "Cancled..!";
+    }
+    clickFor.innerHTML=txt;
     
 }
 
-function closeWind() {
-    myWind.close();
+function windPrompt() {
+    let person = prompt("enter your name:" , "Jhon");
+    let txt;
+
+    if (person === null || person === "") {
+        txt = "No input";
+    }else{
+        txt ="Hello Mr." + person + " welcome";
+    }
+    clickProm.innerHTML= txt ;
+    
 }
