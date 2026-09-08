ced9135d65f9d4b7ace3cb14be71638cd0c8c159
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-09-09 00:35:56 +0600

    done

--- ajax.js
+++ ajax.js
@@ -1,26 +1,39 @@
 const getButton = document.getElementById("get");
 const sendButton = document.getElementById("send");
+getButton.addEventListener("click", getData);
+getButton.addEventListener("click", sendData);
 
-function sendRequest(methood, url) {
+
+function sendRequest(methood, url, data) {
     const promise =new Promise((resolve, reject)=>{
     const xhr = new XMLHttpRequest();
 
     xhr.onload = function () {
+        // for single way
         // console.log(this.response.id);
         // console.log(JSON.parse(this.responseText).id);
         // const valu = JSON.parse(this.responseText);
-        const valu = this.response.id;
-        const valu2 = this.response.title;
-        document.getElementById("demo").innerHTML=`${valu}. ${valu2}`;
+        // const valu = this.response.id;
+        // const valu2 = this.response.title;
+        // document.getElementById("demo").innerHTML=`${valu}. ${valu2}`;
         
-    }
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+        //dynamic way        
+        //error handle condition
+        if(this.status >= 400){
+            reject (`There was an error, check status ${this.status} text is ${this.statusText}`);
+        }else{
+        resolve(this.response);
+        }
+    };
+    xhr.onerror = function () {
+        reject("Error Detect");
+    };
 
+    xhr.open(methood, url);
     xhr.responseType = "json";
-    xhr.send();
-    })
-    
-    
+    xhr.send(data);
+    });
+    return promise;  
 }
 
 function getData() {
@@ -39,12 +52,31 @@ function getData() {
 
     // xhr.responseType = "json";
     // xhr.send();
-    
+    sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/1")
+    .then((responsData)=>{
+      console.log(responsData);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 }
+
 function sendData() {
-    // document.getElementById("demoOne").innerHTML="data sending..."
+    sendRequest(
+        "POST",
+        "https://jsonplaceholder.typicode.com/pots",
+        JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1, 
+    })
+    ).then((responsData)=>{
+      console.log(responsData);
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
     
 }
 
-getButton.addEventListener("click", getData);
-getButton.addEventListener("click", sendData);
