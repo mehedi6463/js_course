08a21b78c99ed722da37ef230a173c40980045f3
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2026-09-22 23:41:53 +0600

    for loop

---
+++ js/forLoop.js
@@ -0,0 +1,24 @@
+//for loop,, for in(get key/index),, for of(get value)
+let i=1;
+for (; ; i++) {
+    if (i<=5) {
+        console.log(i);
+    }else{
+        break;
+    }    
+}
+
+//for of loop value get kore
+let newArray =[1,2,3,4,5,6,7,8,9]
+
+for (v of newArray) {
+    document.getElementById('demo').innerHTML= `Array value is `+ v;
+    // console.log(v)
+    
+}
+
+//for in loop key / index get kore
+for (k in newArray) {
+    // console.log(k);
+    document.getElementById('demo1').innerHTML= `Total Array index is `+ k;
+}
